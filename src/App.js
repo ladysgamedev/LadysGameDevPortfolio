4b3edd753ac7a92784ddaa06ad481a81e07ffbfb
ladymae.manuncia <ladymae.manuncia@tfisolutions.com>
2023-03-13 20:44:12 +0800

    with name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          My Portfolio
+          My Portfolio Lady
         </a>
       </header>
     </div>
